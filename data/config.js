c6a855f696e2e87627c8dbe71ad6e7a788a849b9
opraymind-glitch <o.pray.mind@gmail.com>
2026-06-04 22:55:39 +0900

    Update Rakuten API key

--- data/config.js
+++ data/config.js
@@ -2,5 +2,5 @@
 // Hot Pepper / 楽天トラベルは無料公開APIです
 const CONFIG = {
   hotpepper: "cd8dd041bdb08170",   // Hot Pepper グルメAPIキー（ここに入力）
-  rakuten:   "pk_BkJr3XQyMVWui7cxcMaFyKbqHY4BMoJ6LavLpjmlgyS",   // 楽天トラベルAPIキー（取得後に入力）
+  rakuten:   "93de3ad8-6d46-44fb-b9a8-3d905a354908",   // 楽天トラベルAPIキー（取得後に入力）
 };
